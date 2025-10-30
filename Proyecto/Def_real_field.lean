85a688e975a5fc00e966cf36c3725f73a50ce4b8
Miiike1 <mlabordavelazquez@gmail.com>
2025-10-30 19:22:39 +0100

    terminado archimedean

--- Proyecto/Def_real_field.lean
+++ Proyecto/Def_real_field.lean
@@ -2,7 +2,7 @@ import Mathlib
 
 class RealField (R : Type) extends SupSet R, Field R, LinearOrder R, IsStrictOrderedRing R where
   sSup_axiom :  ∀ (S: Set R), S.Nonempty → BddAbove S → IsLUB S (sSup S)
-variable (X : Type) [RealField X]
+variable {X : Type} [RealField X]
 
 #print RealField
 
@@ -11,42 +11,42 @@ open RealField
 --print para ver teoremas, axiomas y props dados por la clase
 --escribo teoremas útliles para el futuro
 theorem supA_lt_x_in_upbdA {A : Set X} (a : X) (hA : A.Nonempty)
-(hB : BddAbove A):a ∈ upperBounds A → sSup A ≤ a  := by
+ (hB : BddAbove A) : a ∈ upperBounds A → sSup A ≤ a  := by
   intro ha
-  have sup_islub:= sSup_axiom A hA hB
-  rw[IsLUB,IsLeast, lowerBounds] at sup_islub
-  obtain ⟨hsup1,hsup2⟩:=sup_islub
-  simp at hsup2
-  have hsup3:= hsup2
-  exact hsup3 ha
+  exact (sSup_axiom A hA hB).2 ha
 
-
-
+theorem gt_sup_then_upbdd {A : Set X} (x : X) (hA : A.Nonempty)
+ (hB : BddAbove A) : sSup A < x → x ∈ upperBounds A:= by
+  have supp:= (sSup_axiom A hA hB).1
+  rw[upperBounds] at supp
+  simp at supp
+  intro hx
+  apply le_of_lt at hx
+  intro a ha
+  have sup2:= supp ha
+  exact le_trans (supp ha) hx
 
 theorem supA_lt_a_in_A {A : Set X} (hA : A.Nonempty)
  (hB : BddAbove A) (a : X) :a ∈ A → a ≤ sSup A := by
   intro ha
-  obtain ⟨sup_up,sup_low⟩:= sSup_axiom A hA hB
-  exact sup_up ha
+  exact (sSup_axiom A hA hB).1 ha
 
 theorem x_lt_supA_a_lt_sup {A : Set X} (hA : A.Nonempty)
- (hB : BddAbove A) (b : X): b ≤ sSup A → ∃ a ∈ A, b ≤ a := by
-
+ (hB : BddAbove A) (b : X) : b < sSup A → ∃ a ∈ A, b < a := by
   intro hx
   by_contra hc
   push_neg at hc
   have x_is_upper_bound: b ∈ upperBounds A
   · have hc' : ∀ x₁ ∈ A, x₁ ≤ b
     · intro x₁ hx₁
-      exact le_of_lt (hc x₁ hx₁)
+      specialize hc x₁ hx₁
+      exact hc
     exact hc'
-  sorry
+  have sup_lt_b := supA_lt_x_in_upbdA b hA hB x_is_upper_bound
+  linarith
 
---he tenido problemas a la hora de cerrar este teorema, no me deja aplicar el teorema
--- supA_lt_a_in_A
---al poner tanto "have sup_lt_x:= supA_lt_a_in_A x hA hB x_in:upper_bound
---como distintas variaciones (probando a introducir A, quitar x etc) me da error.
---no sé cómo solucionarlo (estoy creando estos teoremas porque creo que vendrán bien)
+
+
 
 instance : Archimedean X where
   arch := by
@@ -54,7 +54,7 @@ instance : Archimedean X where
     by_contra hc
     have forall_n_prod_lt_x:  ∀(m : ℕ), m • y ≤ x := by
       intro m
-      rw[not_exists] at hc
+      push_neg at hc
       specialize hc m
       linarith
     by_cases neg: x≤0
@@ -82,19 +82,7 @@ instance : Archimedean X where
       have sup_sub_y_lt_x: sup - y < sup
       · rw[sub_lt_self_iff]; exact hy1
       have alph_in_A_gt_sup_sub_y: ∃α ∈ A, sup - y < α
-      · by_contra hc
-        have sup_sub_y_upbd: sup - y ∈ upperBounds A
-        · intro x hx
-          push_neg at hc
-          specialize hc x
-          exact hc hx
-        have sup_lt_sup_sub_y: sup < sup - y
-        · rw[IsLUB,IsLeast, lowerBounds] at sup_islub
-          obtain ⟨hsup1,hsup2⟩:=sup_islub
-          simp at hsup2
-          have hsup3:= hsup2 sup_sub_y_upbd
-          linarith
-        linarith
+      · exact  x_lt_supA_a_lt_sup Anonempty Abddabv (sup-y) sup_sub_y_lt_x
       obtain ⟨α,hα1,hα2⟩:=alph_in_A_gt_sup_sub_y
       have alpha_eq_n0_y : ∃(n₀ : ℕ), α = n₀ • y
       · exact hα1
@@ -106,8 +94,12 @@ instance : Archimedean X where
         exact hα2
       have n0_add_1_in_A: (n₀ + 1) • y ∈ A:= by
         use (n₀ +1)
-      have sup_gt_n0_add_one_y:
+      have n0_add_one_lt_sup:= supA_lt_a_in_A Anonempty Abddabv
+      specialize n0_add_one_lt_sup ((n₀ + 1) • y) n0_add_1_in_A
+      linarith
 
+
+      --quedó más larga de lo que querría, pero bueno, de momento la dejo así
 
 
 
